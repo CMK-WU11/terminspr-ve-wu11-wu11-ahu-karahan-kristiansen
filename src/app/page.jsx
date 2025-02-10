e579e504dd0558492a539b6e1008101d8da37508
wu11-ahu-karahan-kristiansen <u87327@rts-365.dk>
2025-02-10 10:03:00 +0100

    installed scss, added color variables and removed/changed pre-installed stylesheets. I also added an assignment folder.

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import "@/styles/globals.scss"
+
+export default function Home() {
+  return (
+    <main>
+      <p>Test</p>
+    </main>
+  );
+}
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,9 @@
+export default function Nav(){
+    return(
+        <nav>
+            <Link to= '/home'></Link>
+            <Link to= '/search'></Link>
+            <Link to= '/calender'></Link>
+        </nav>
+    )
+}
